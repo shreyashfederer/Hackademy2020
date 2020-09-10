f20b2108a7c30b72e8ef630e1eb4e48f5d5dbfe8
bluekap <vabg96@gmail.com>
2020-09-10 17:54:00 +0530

    Added ocr from file

--- ocr.py
+++ ocr.py
@@ -1,10 +1,11 @@
 from google.cloud import vision
+from google.cloud.vision import types
 
-def get_text_from_image(url):
+
+def get_text_from_url(url):
     client = vision.ImageAnnotatorClient.from_service_account_json('/home/meenalgoswami115/credentials.json')
     image = vision.types.Image()
     image.source.image_uri = url
-
     response = client.text_detection(image=image)
     texts = response.text_annotations
     return texts
@@ -23,6 +24,15 @@ def get_text_from_image(url):
     #         '{}\nFor more info on error messages, check: '
     #         'https://cloud.google.com/apis/design/errors'.format(
     #             response.error.message))
+
+def get_text_from_file(file_path):
+    client = vision.ImageAnnotatorClient.from_service_account_json('/home/meenalgoswami115/credentials.json')
+    with io.open(file_path, 'rb') as image_file:
+        content = image_file.read()
+    image = types.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    return texts
 
 if __name__ == "__main__":
 	get_text_from_image("gs://images-hackathon-288506/images/billing-invoice-with-payment-plan.png")
