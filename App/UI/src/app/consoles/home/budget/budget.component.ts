027dfa0ab162bb341d50b453267eb7f9e45ecc11
kr58 <pawankumar1122.pk@gmail.com>
2020-09-12 02:19:13 +0530

    Budget

--- App/UI/src/app/consoles/home/budget/budget.component.ts
+++ App/UI/src/app/consoles/home/budget/budget.component.ts
@@ -56,7 +56,6 @@ export class BudgetComponent implements OnInit {
   save() {
     this.HomeService.addBudget("Pawan", this.rooms, this.total).subscribe((data) => {
       console.log(data)
-    })    
+    })
   }
-
 }
