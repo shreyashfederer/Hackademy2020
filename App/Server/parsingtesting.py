aee5cd25e3d82616e139a565f98a23a63703cced
bluekap <vabg96@gmail.com>
2020-09-12 15:18:30 +0530

    OCr

--- App/Server/parsingtesting.py
+++ App/Server/parsingtesting.py
@@ -21,8 +21,9 @@ def get_products(text):
             #    print(points)
             #    print(points[0]['y'])
             #    print(points[2]['y'])
-               y1 = vertices[0].y
-               y2 = vertices[2].y
+              temp_list = list(vertices)
+               y1 = temp_list[0].y
+               y2 = temp_list[2].y
                print(y1 + "" + y2 + "################################################")
                print("\n")
                for cmppoints in value:
