32af3da97638800e3eb9c670e8c9b1a866118af4
bluekap <vabg96@gmail.com>
2020-09-12 15:26:30 +0530

    OCr

--- App/Server/parsingtesting.py
+++ App/Server/parsingtesting.py
@@ -20,7 +20,7 @@ def get_products(text):
            y2 = value.bounding_poly.vertices[2].y
            print(y1,y2)
            print("\n")
-           for cmppoints in value:
+           for cmppoints in text:
                if(cmppoints.bounding_poly.vertices[0]['y'] in range(y1-20,y1+20) and cmppoints.bounding_poly['vertices'][2]['y'] in range(y2-20,y2+20)):
                 #    print("Inside block: ")
                 #    print(cmppoints['description'],ord(cmppoints['description'][0]))
